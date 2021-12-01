f11a5eb94abd9a6b8775d358785bb48635e9bd8a
Gabriel Diaconița <diaconitagabriel@gmail.com>
2021-12-01 07:25:17 +0200

    Add solution for day1

--- days/1/Day1.h
+++ days/1/Day1.h
@@ -28,13 +28,50 @@ public:
 
   long DoWork1()
   {
-    return 0;
+    LL ret = 0;
+    LL prev = -1;
+    for (auto d : mData)
+    {
+      LL dd = stoll(d);
+      if (prev >= 0 && dd > prev )
+        ret++;
+     
+      prev = stoll(d);
+    }
+    return ret;
   }
 
   long DoWork2()
   {
-
-    return 0;
+    vector<pair<int, LL>> windows;
+    
+    for (auto d : mData)
+    {
+      LL dd = stoll(d);
+      
+      for (auto & pp : windows)
+      {
+        if (pp.first < 3)
+        {
+          pp.second += dd;
+        }
+        pp.first += 1;
+      }
+      
+      windows.push_back(make_pair(1, dd));
+    }
+    
+    LL ret = 0, prev = -1;
+    for (auto d : windows)
+    {
+      if (prev >= 0 && d.second > prev )
+        ret++;
+     
+      prev = d.second;
+    }
+    windows.erase(windows.end() - 1);
+    windows.erase(windows.end() - 1);
+    return ret;
   }
   
   string Part1() override

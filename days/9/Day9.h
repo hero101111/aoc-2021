b1059215f9062e25e246711052ae35b8f76ebfb1
Gabriel Diaconița <diaconitagabriel@gmail.com>
2021-12-09 07:22:25 +0200

    p1 day 9

--- days/9/Day9.h
+++ days/9/Day9.h
@@ -8,6 +8,7 @@ class Day9 : public ISolutionDay
 private:
 
   vector<string> mData;
+  using DT = set<LL>;
 
 public:
 
@@ -28,8 +29,59 @@ public:
 
   long DoWork1()
   {
-
-    return 0;
+    LL ret = 0;
+    DynamicMap<int> m;
+    
+    int crtLine = 0;
+    for (auto d : mData)
+    {
+      int crtCol = 0;
+      for (char c : d)
+      {
+        string strVal(1, c);
+        int val = stoi(strVal);
+        m[{crtCol, crtLine}] = val;
+        crtCol += 1;
+      }
+      crtLine +=1;
+    }
+    //    auto [x1, y1, x2, y2] = RegExMatch4(d, R"((\d+),(\d+) -> (\d+),(\d+))");
+    
+      for (auto y : m.range_y())
+    {
+      for (auto x : m.range_x())
+      {
+        bool isLow = true;
+        Point p{x, y};
+        int crtVal = m[p];
+        auto neigh = p.GetNeighbours();
+        
+       // vector<int> vals;
+        
+        for (auto n : neigh)
+        {
+          if (abs(n.x - x) + abs( n.y - y) > 1)
+            continue;
+          
+          
+          int nVal = 0;
+          bool bb = m.at(n, &nVal);
+          //vals.push_back(nVal);
+          if (bb)
+          {
+            if (nVal <= crtVal)
+              isLow = false;
+          }
+          else
+            continue;
+        }
+        
+        if (isLow)
+          ret += crtVal + 1;
+      }
+    }
+    cout <<ret << endl;
+    return ret;
   }
 
   long DoWork2()
@@ -54,6 +106,8 @@ public:
 
   bool Test() override
   {
+    mCurrentInput = "test";
+    Part1();
     return true;
   }
 };

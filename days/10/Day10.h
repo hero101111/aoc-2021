6c2891a1c71349c1e366a3f5c9b80cba293e54dc
Gabriel Diaconita <diaconitagabriel@gmail.com>
2021-12-10 07:18:41 +0200

    sdfsdf

--- days/10/Day10.h
+++ days/10/Day10.h
@@ -7,6 +7,7 @@ class Day10 : public ISolutionDay
 {
 private:
 
+  using DT = set<int>;
   vector<string> mData;
 
 public:
@@ -28,8 +29,66 @@ public:
 
   long DoWork1()
   {
+    /*string text2 = RegexReplace(text, R"(\d)", "more than $1");
+    auto [kv] = RegExMatch1(text, R"(we have (\d) monkeys)");
+    fprintf(KVERBOSE, "hello man " + kv);
+    fprintf(KVERBOSE, text2); */
+    LL ret = 0;
 
-    return 0;
+    set<char> fs = { '(', '[', '{', '<' };
+  
+    for (auto d : mData)
+    {
+      bool isValid = true;
+
+      stack<char> st;
+      for (char c : d)
+      {
+        if (setContains(fs, c))
+          st.push(c);
+        else
+        {
+          char sTop = st.top();
+          st.pop();
+          if (sTop == '(' && c != ')')
+          {
+            isValid = false;
+          }
+
+          if (sTop == '{' && c != '}')
+          {
+            isValid = false;
+          }
+          if (sTop == '<' && c != '>')
+          {
+            isValid = false;
+          }
+          if (sTop == '[' && c != ']')
+          {
+            isValid = false;
+          }
+          if (!isValid && c == ')')
+            ret += 3;
+          if (!isValid && c == ']')
+            ret += 57;
+          if (!isValid && c == '}')
+            ret += 1197;
+          if (!isValid && c == '>')
+            ret += 25137;          
+        }
+        if (!isValid)
+          break;
+      }
+
+      if (!isValid)
+      {
+       // cout << d << endl;
+        continue;
+      }
+
+    }
+    cout << mCurrentInput << " P1 " << ret << endl;
+    return ret;
   }
 
   long DoWork2()
@@ -54,6 +113,8 @@ public:
 
   bool Test() override
   {
+    mCurrentInput = "test";
+    Part1();
     return true;
   }
 };
